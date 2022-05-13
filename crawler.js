3541da1a03168bfc1862a20f0e54057ff40e9e4d
tomerfok <tomer.fok@gmail.com>
2022-05-13 18:52:20 +0300

    trying to work out queryselector to get all hrefs and imgs

--- crawler.js
+++ crawler.js
@@ -1,11 +1,16 @@
 import createBrowserless from 'browserless';
 import getHTML from 'html-get';
-import parse from 'node-html-parser';
+import jsdom from "jsdom";
+import nodeHtmlParser from 'node-html-parser';
+const { parse } = nodeHtmlParser;
+const { JSDOM } = jsdom;
 
 let args = process.argv.slice(2);
 
 let url = args[0];
 let depth = +args[1];
+let content;
+let root;
 
 const browserlessFactory = createBrowserless();
 process.on('exit', () => {
@@ -20,12 +25,23 @@ const getContent = async url => {
     return result;
 };
 
+const dom = new JSDOM(url, { includeNodeLocations: true });
+const document = dom.window.document;
+const hrefEl = document.querySelectorAll("href");
+const imgEl = document.querySelectorAll("img");
+
+
 getContent(url)
     .then(content => {
-        console.log(content)
-        process.exit()
+        content = content;
+        console.log(content);
+
+        root = parse(content.html);
+        console.log(root);
+
+        process.exit();
     })
     .catch(error => {
-        console.error(error)
-        process.exit(1)
+        console.error(error);
+        process.exit(1);
     });
